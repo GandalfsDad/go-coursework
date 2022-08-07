6a3a95273bf7f77638ff5721d87b8a8fc8652017
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-08-08 06:37:00 +1000

    Add languageBot interface

--- maps/interfaces/main.go
+++ maps/interfaces/main.go
@@ -2,25 +2,24 @@ package main
 
 import "fmt"
 
-type englishBot struct{}
+type languageBot interface {
+	getGreeting() string
+}
 
-//type spanishBot struct{}
+type englishBot struct{}
+type spanishBot struct{}
 
 func main() {
-	//sb := spanishBot{}
+	sb := spanishBot{}
 	eb := englishBot{}
 
 	printGreeting(eb)
-	//printGreeting(sb)
+	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func printGreeting(lb languageBot) {
+	fmt.Println(lb.getGreeting())
 }
-
-// func printGreeting(sb spanishBot) {
-// 	fmt.Println(sb.getGreeting())
-// }
 
 func (englishBot) getGreeting() string {
 	//Some voodoo for English greeting
