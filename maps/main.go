d6acdf3c3eb4cd4aa61e23be41afb62771ff76f2
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-08-07 18:26:13 +1000

    Add basic map

---
+++ maps/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Alternative Creates
+	//var colours map[string]string
+	//colours := make(map[string]string)
+
+	colours := map[string]string{
+		"red":   "#ff0000",
+		"blue":  "#0000ff",
+		"green": "#00ff00",
+	}
+
+	//Add something
+	colours["white"] = "#ffffff"
+
+	//Remove Something
+	delete(colours, "white")
+
+	fmt.Println(colours)
+}
