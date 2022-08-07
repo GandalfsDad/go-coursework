17031295ebcebb836e284e85172c52107d94c1ca
GandalfsDad <rhianmcclelland193@hotmail.com>
2022-08-07 18:39:54 +1000

    Feature/maps (#4)
    * Add basic map
    
    * Add looping function
    
    * Remove extra print

---
+++ maps/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Alternative Creates
+	//var colours map[string]string
+	//colours := make(map[string]string)
+
+	colours := map[string]string{
+		"red":   "#ff0000",
+		"blue":  "#0000ff",
+		"green": "#00ff00",
+	}
+
+	//Add something
+	colours["white"] = "#ffffff"
+
+	//Remove Something
+	delete(colours, "white")
+
+	printMap(colours)
+}
+
+func printMap(c map[string]string) {
+	for colour, hex := range c {
+		fmt.Println("Hex code for", colour, "is", hex)
+	}
+}
