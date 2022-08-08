f6573d2826a6907b0f45ca6addc7ae2e29720807
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-08-08 18:05:06 +1000

    Add printArea Implementation

--- Assignment2/main.go
+++ Assignment2/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type shape interface {
 	getArea() float64
 }
@@ -13,7 +15,11 @@ type square struct {
 }
 
 func main() {
+	s := square{sideLength: 5.5}
+	t := triangle{base: 5.4, height: 6.5}
 
+	printArea(s)
+	printArea(t)
 }
 
 func (t triangle) getArea() float64 {
@@ -22,4 +28,9 @@ func (t triangle) getArea() float64 {
 
 func (s square) getArea() float64 {
 	return s.sideLength * s.sideLength
+}
+
+func printArea(s shape) {
+	a := s.getArea()
+	fmt.Println("Area is ", a)
 }
