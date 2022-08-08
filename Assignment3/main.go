62c0185b96f5448aada5eb697f934def4212c99a
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-08-08 18:17:12 +1000

    Add Check for arg count

--- Assignment3/main.go
+++ Assignment3/main.go
@@ -6,5 +6,14 @@ import (
 )
 
 func main() {
-	fmt.Println(os.Args)
+	if len(os.Args) == 1 {
+		fmt.Println("Need to provide a filepath")
+		os.Exit(1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many filepaths provided")
+		os.Exit(1)
+	}
+
+	l := os.Args[1]
+	fmt.Println(l)
 }
