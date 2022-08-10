82df4250b48893428c14f118512975912466963b
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-08-10 10:02:03 +1000

    Add initial channel implementtion

--- channels/main.go
+++ channels/main.go
@@ -14,18 +14,23 @@ func main() {
 		"http://amazon.org",
 	}
 
+	c := make(chan string)
+
 	for _, website := range websites {
-		fmt.Println(website)
+		go checkLink(website, c)
 	}
+
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
-
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "Yep its up"
 }
